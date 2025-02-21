213e8a446a4ad982d97457baa12d409de09f5246
uzevie George <uzeviegeorge@gmail.com>
2025-02-21 01:44:59 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,76 @@
+import requests
+
+api_key = "" #your api key
+country = "nigeria"
+state = "rivers"
+city = "Port Harcourt"
+longitude = ""
+latitude = ""
+station_name = ""
+
+
+# to get general data from the api
+
+# url = f"https://api.airvisual.com/v2/nearest_city?key={api_key}"
+
+
+
+# GET List of supported countries
+
+# url = f"http://api.airvisual.com/v2/countries?key={api_key}"
+
+
+
+
+#GET List of supported states in a country
+# url = f"http://api.airvisual.com/v2/states?country={country}&key={api_key}"
+
+
+#GET List of supported cities in a state
+
+# url = f"http://api.airvisual.com/v2/cities?state={state}&country={country}&key={api_key}"
+
+
+
+#GET nearest city data (IP geolocation)
+# url = f"http://api.airvisual.com/v2/nearest_city?key={api_key}"
+
+
+
+#Get nearest city data (GPS coordinates)
+# url = f"http://api.airvisual.com/v2/nearest_city?lat={latitude}&lon={longitude}&key={api_key}"
+
+
+# GET specified city data
+# url = f"http://api.airvisual.com/v2/city?city={city}&state={state}&country={country}&key={api_key}"
+
+
+
+# GET List of supported stations in a city
+# url = f"http://api.airvisual.com/v2/stations?city={city}&state={state}&country={country}&key={api_key}"
+
+
+# GET nearest station data (IP geolocation)
+# url = f"http://api.airvisual.com/v2/nearest_station?key={api_key}"
+
+#Get nearest station data (GPS coodinates)
+# url = f"http://api.airvisual.com/v2/nearest_station?lat={latitude}&lon={longitude}&key={api_key}"
+
+# Get specified station data
+# url = f"http://api.airvisual.com/v2/station?station={station_name}&city={city}&state={state}&country={country}&key={api_key}"
+
+
+# Get global city ranking
+# url = f"http://api.airvisual.com/v2/city_ranking?key={api_key}&sort=&country={country}"
+
+
+def get_result(test_url):
+
+    response = requests.get(test_url)
+    data = response.json()
+    print(data) 
+
+
+
+
+get_result(url)    
